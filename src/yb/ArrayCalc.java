d569041411edd979efb44325a00ba37f5da1af61
isaacblack <isaacblack234@gmail.com>
2020-10-15 15:28:47 -0400

    Initial Commit with method shells

---
+++ src/yb/ArrayCalc.java
@@ -0,0 +1,21 @@
+package yb;
+
+public class ArrayCalc {
+	
+	public int getSum(int[] arr) {
+		return 0;
+	}
+	
+	public int getMaxValue(int[] arr) {
+		return 0;
+	}
+	
+	public int getMinValue(int[] arr) {
+		return 0;
+	}
+	
+	public int getRange(int[] arr) {
+		return 0;
+	}
+	
+}
